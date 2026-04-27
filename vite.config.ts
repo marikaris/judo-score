6b5145924e9b750132ee8b511140c17b685ea566
Vincent Free <vincentfree@outlook.com>
2026-04-27 23:35:45 +0200

    Adding electorn as a deployment target. With electron, the application can run stand-alone without a browser or internet access. It also removes the need of a browser, making it as native as we can go.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,35 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import electron from 'vite-plugin-electron/simple'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({ mode }) => {
+  const isElectron = process.env.ELECTRON === 'true'
+
+  return {
+    plugins: [
+      vue(),
+      isElectron && electron({
+        main: {
+          // Shortcut of `build.lib.entry`.
+          entry: 'electron/main.ts',
+        },
+        preload: {
+          // Shortcut of `build.rollupOptions.input`.
+          // Preload scripts may contain Web assets, so use `build.rollupOptions.input` instead `build.lib.entry`.
+          input: 'electron/preload.ts',
+        },
+        // Ployfill the Electron and Node.js built-in modules for Renderer process.
+        // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+        renderer: {},
+      }),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: isElectron ? './' : '/'
   }
 })
