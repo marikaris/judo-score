02ff832dbbda8bd598311c1a307918b31698af23
Vincent Free <vincentfree@outlook.com>
2026-05-09 23:40:54 +0200

    add logo's, update renderer

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron/simple'
+import renderer from 'vite-plugin-electron-renderer'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -20,10 +21,10 @@ export default defineConfig(({ mode }) => {
           // Preload scripts may contain Web assets, so use `build.rollupOptions.input` instead `build.lib.entry`.
           input: 'electron/preload.ts',
         },
-        // Ployfill the Electron and Node.js built-in modules for Renderer process.
-        // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-        renderer: {},
       }),
+      // Ployfill the Electron and Node.js built-in modules for Renderer process.
+      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+      isElectron && renderer(),
     ].filter(Boolean),
     resolve: {
       alias: {
